617629eec326eda5f88a9a21850bc95a4848b591
Ian H. Kessler <ianholm9@gmail.com>
2023-10-18 10:35:43 -0600

    ..

---
+++ DataDictionary.jl
@@ -0,0 +1,21 @@
+module DataDictionary
+
+function soybean()
+    name = "SoyBean"
+    file = "raw_data/soybean-small.csv"
+    columns = [
+        "Date", 
+        "Plant-Stand", 
+        "Temp", 
+        "Hail", 
+        "Crop-Hist", 
+        "Area-Damaged", 
+        "Severity", 
+        "Seed-TMT", 
+        "Germination", 
+        "Plant-Growth", 
+        "Leaves", 
+        "Leafspots-Halo", 
+        "Leafspot-Size", 
+        "Leaf"
+    ]
